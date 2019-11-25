23d8b3c20503e1558a0b690a505dea17f67e383e
the 14th web <dennis.itua1@gmail.com>
2019-11-25 16:27:50 -0500

    port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var path = require("path");
 
 // Express setup
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 1414;
 
 // Middleware
 app.use(methodOverride("_method"));
