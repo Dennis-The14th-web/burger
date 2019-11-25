4168eaa2751c40d4771cc5c268a7cba4f6a37f10
the 14th web <dennis.itua1@gmail.com>
2019-11-25 13:10:00 -0500

    updating all functionl details for app

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+### Populate data with 3 starter burgers
+INSERT INTO burgers (burger_name) VALUES ('Quater Pounder Cheeseburger');
+
+INSERT INTO burgers (burger_name) VALUES ('Big Mac Cheeseburger');
+
+INSERT INTO burgers (burger_name) VALUES ('Junior Cheeseburger');
