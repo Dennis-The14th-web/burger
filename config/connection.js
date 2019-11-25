2bcb645d1820544c51c08c3411ec259ea45d59ed
the 14th web <dennis.itua1@gmail.com>
2019-11-25 16:33:24 -0500

    connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,28 +1,53 @@
-// Dependencies
+// // Dependencies
+// var mysql = require("mysql");
+
+// var connection;
+
+// // Connect with JawsDB database on our Heroku app
+// // 	If not found, then run locally
+// if (process.env.JAWSDB_URL) {
+// 	connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+// 	connection = mysql.createConnection({
+// 	  host: "192.168.99.100",
+// 	  user: "root",
+// 	  password: "docker",
+// 	  database: "burgers_db"
+// 	});
+// }
+
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
+
+// // Export our MySQL connection
+// module.exports = connection;
+
+
+
+// Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection;
+var connection = mysql.createConnection({
+  port: 3306,
+  host: "192.168.99.100",
+  user: "root",
+  password: "docker",
+  database: "burgers_db"
+});
 
-// Connect with JawsDB database on our Heroku app
-// 	If not found, then run locally
-if (process.env.JAWSDB_URL) {
-	connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-	connection = mysql.createConnection({
-	  host: "192.168.99.100",
-	  user: "root",
-	  password: "docker",
-	  database: "burgers_db"
-	});
-}
-
+// Make connection.
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
   }
-  console.log("connected as id " + connection.threadId);
+  console.log("connected as id: " + connection.threadId);
 });
 
-// Export our MySQL connection
+// Export connection for our ORM to use.
 module.exports = connection;
