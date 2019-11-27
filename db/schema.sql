82599eaea7d9b2258301beb1bc2e54036a3f1456
the 14th web <dennis.itua1@gmail.com>
2019-11-27 04:20:37 -0500

    deployment corrections

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,11 @@
-
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
-	burger_name VARCHAR(255) NOT NULL,
-	devoured BOOLEAN NOT NULL DEFAULT 0,
-	date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	burger_name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,3 @@
-INSERT INTO burgers (burger_name) VALUES ('Quater Pounder Cheeseburger');
-
-INSERT INTO burgers (burger_name) VALUES ('Big Mac Cheeseburger');
-
-INSERT INTO burgers (burger_name) VALUES ('Junior Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Hamburger');
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Double Meat');
